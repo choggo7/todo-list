83c2e22e0abb38850b25be36b810071cb878b305
Fajr abdelali <afajr@ayaline.com>
2022-06-08 18:56:15 +0100

    add functionality of list items

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 
-export const TodoList = () =>{
+export const TodoList = (props:TodoListInterface) =>{
   return(
     <div className="todo__list">
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
+
+      <ul>
+       { props.todos.map((todo) => ( <li key={todo.id} >  <TodoItem todo={todo} handleTodoBlur={props.handleTodoBlur} handleTodoComplete={props.handleTodoComplete} handleTodoRemove={props.handleTodoRemove} handleTodoUpdate={props.handleTodoUpdate} />  </li> ) ) }
+      </ul>
+
     </div>
   )
 }
