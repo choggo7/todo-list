31d293db1b2fbd56f76141c637c3d78f64eda2c5
Fajr abdelali <afajr@ayaline.com>
2022-06-08 18:43:18 +0100

    add the functionality of todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,7 +4,12 @@ const TodoItem = (props: TodoItemInterface) => {
   return (
     <div className="todo__item">
 
-      
+      <input type="text" value={props.todo.text} onBlur={props.handleTodoBlur} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { props.handleTodoUpdate(e, props.todo.id) }} className="todo__item--input" />
+      TodoItem !
+      <div className="todo__item--container" onClick={() => { props.handleTodoComplete(props.todo.id) }} >
+        {props.todo.isCompleted ? (<span className="todo__item--checked">✔</span>) : (<span className="todo__item--unchecked"></span>)}
+      </div>
+      <div className="todo__item--remove" onClick={() => { props.handleTodoRemove(props.todo.id) }}>⨯</div>
 
     </div>
   )
