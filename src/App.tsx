23f2e9b54ad63862853fba0cb725dff741c8abda
Fajr abdelali <afajr@ayaline.com>
2022-06-08 19:46:25 +0100

    add shortid package to generate id,and add the functionelity of todo form

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { useState, FC } from 'react';
 import './App.css';
 import { TodoList } from './components/TodoList';
 
-const initialeTodos: TodoInterface[] = [{ id: 1, text: "my first task", isCompleted: true },
-{ id: 1, text: "my first task", isCompleted: true },
-{ id: 1, text: "my first task", isCompleted: true },
-{ id: 1, text: "my first task", isCompleted: false }]
+const initialeTodos: TodoInterface[] = [{ id: "1", text: "my first task", isCompleted: true },
+{ id: "1", text: "my first task", isCompleted: true },
+{ id: "1", text: "my first task", isCompleted: true },
+{ id: "1", text: "my first task", isCompleted: false }]
 const App: FC = () => {
 
 const [todos, setTodos] = useState<TodoInterface[]>(initialeTodos)
@@ -14,7 +14,7 @@ const [todos, setTodos] = useState<TodoInterface[]>(initialeTodos)
 
   return (
     <div className="App">
-      <TodoList />
+      <TodoList todos={todos}  />
     </div>
   );
 }
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import shortid from 'shortid'
 
-function TodoForm() {
+export const TodoForm = (props: TodoFormInterface) => {
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const [formState, setFormState] = useState('')
+
+  const hundleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState(e.target.value)
+  }
+
+  const hundleInputEnter = (e:React.KeyboardEvent) => {
+    if(e.key === "Enter"){
+      const newTodo :TodoInterface = {
+        id:shortid.generate(),
+        text: formState,
+        isCompleted:false
+      }
+      props.handleTodoCreate(newTodo)
+
+      if(inputRef && inputRef.current){
+        inputRef.current.value = ""
+      }
+
+    }
+
+
+  }
+
+
+
   return (
-    <div>Todo</div>
+    <div className="todo__form">
+      <input type="text" placeholder='Add new task' className="todo__text" onChange={ e => hundleInputChange(e)} onKeyPress={ e=> hundleInputEnter(e) } />
+    </div>
   )
 }
 
-export default TodoForm
