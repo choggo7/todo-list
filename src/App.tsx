9ca7807d5ac178a9e1414f55ab74a92f1f9cd2f8
Fajr abdelali <afajr@ayaline.com>
2022-06-09 02:18:50 +0100

    add animation

--- src/App.tsx
+++ src/App.tsx
@@ -6,53 +6,57 @@ import { TodoList } from './components/TodoList';
 
 const App = () => {
 
-const [todos, setTodos] = useState<TodoInterface[]>([])
+  const [todos, setTodos] = useState<TodoInterface[]>([])
+  const globalStateContext = React.createContext(todos);
 
-const handleTodoCreate =(todo: TodoInterface) => {
-  const newTodosState: TodoInterface[] = [...todos]
-  newTodosState.push(todo)
-  setTodos(newTodosState)
-}
-
-const handleTodoUpdate =(event: React.ChangeEvent<HTMLInputElement>, id: string) => {
-  const newTodosState: TodoInterface[] = [...todos]
-  newTodosState.find((todo: TodoInterface) => todo.id === id)!.text = event.target.value
-  setTodos(newTodosState)
-}
-
-const handleTodoRemove = (id: string) => {
-  const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)
-  setTodos(newTodosState)
-}
-
-const handleTodoComplete = (id: string) => {
-  const newTodosState: TodoInterface[] = [...todos]
-  newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted
-  setTodos(newTodosState)
-}
-
-const handleTodoBlur = (event: React.ChangeEvent<HTMLInputElement>) =>{
-  if (event.target.value.length === 0) {
-    event.target.classList.add('todo-input-error')
-  } else {
-    event.target.classList.remove('todo-input-error')
+  const handleTodoCreate = (todo: TodoInterface) => {
+    const newTodosState: TodoInterface[] = [...todos]
+    newTodosState.push(todo)
+    setTodos(newTodosState)
   }
-}
+
+  const handleTodoUpdate = (event: React.ChangeEvent<HTMLInputElement>, id: string) => {
+    const newTodosState: TodoInterface[] = [...todos]
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.text = event.target.value
+    setTodos(newTodosState)
+  }
+
+  const handleTodoRemove = (id: string) => {
+    const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)
+    setTodos(newTodosState)
+  }
+
+  const handleTodoComplete = (id: string) => {
+    const newTodosState: TodoInterface[] = [...todos]
+    newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted
+    setTodos(newTodosState)
+  }
+
+  const handleTodoBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length === 0) {
+      event.target.classList.add('todo-input-error')
+    } else {
+      event.target.classList.remove('todo-input-error')
+    }
+  }
 
 
 
   return (
     <div className="App">
-      <p className='description' >Add your task and hit enter 😎</p>
+
       <TodoForm todos={todos} handleTodoCreate={handleTodoCreate} />
-      <TodoList 
-      todos={todos}
-      handleTodoUpdate={handleTodoUpdate}
-      handleTodoRemove={handleTodoRemove}
-      handleTodoComplete={handleTodoComplete}
-      handleTodoBlur={handleTodoBlur}
+      
+      <TodoList
+        todos={todos}
+        handleTodoUpdate={handleTodoUpdate}
+        handleTodoRemove={handleTodoRemove}
+        handleTodoComplete={handleTodoComplete}
+        handleTodoBlur={handleTodoBlur}
       />
 
+      <p className='description' >Add your task and hit enter 😎</p>
+      <p>you can modifie your task directly</p>
       <ul className="about">
         <li>About</li>
         <li className='uncomplet'><span>⚪</span> task not complet</li>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,24 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import TodoItem from './TodoItem'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-export const TodoList = (props:TodoListInterface) =>{
-  const [todos,setTodos] = useState<TodoInterface[]>([])
+export const TodoList = (props: TodoListInterface) => {
+  //const [todos, setTodos] = useState<TodoInterface[]>([])
+  
 
-  return(
+  return (
     <div className="todo__list">
 
       <ul>
-       { props.todos.map((todo) => ( <li key={todo.id} >  <TodoItem todo={todo} handleTodoBlur={props.handleTodoBlur} handleTodoComplete={props.handleTodoComplete} handleTodoRemove={props.handleTodoRemove} handleTodoUpdate={props.handleTodoUpdate} />  </li> ) ) }
+        <TransitionGroup component="ul">
+          {props.todos.map((todo) => (
+            <CSSTransition key={todo.id} timeout={700} classNames="item">
+              <li key={todo.id} >
+                <TodoItem todo={todo} handleTodoBlur={props.handleTodoBlur} handleTodoComplete={props.handleTodoComplete} handleTodoRemove={props.handleTodoRemove} handleTodoUpdate={props.handleTodoUpdate} />
+              </li>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       </ul>
 
     </div>
